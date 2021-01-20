a1df1dcf32cd8f44c6856957c573651504063e61
Xueying Qin <qinxy1995@gmail.com>
2021-01-20 03:16:38 +0000

    add tests

--- rust_containers/src/lib.rs
+++ rust_containers/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(const_fn)] // enabling impl<T: PartialEq> 
+//#![feature(const_fn)] // enabling impl<T: PartialEq> 
 
 mod vectors;
 
@@ -12,6 +12,12 @@ mod tests {
     }
 
     #[test]
+    fn unique_creation_with_capacity_works() {
+        let vec = UniqueVec::<u32>::with_capacity(10);
+        assert_eq!(vec.capacity(), 10);
+    }
+
+    #[test]
     fn unique_push_works() {
         let mut vec = UniqueVec::new();
         for x in 0..10000 {
@@ -19,5 +25,36 @@ mod tests {
             vec.push(x);
         }
         assert_eq!(vec.len(), 10000); // no duplication
+    }
+
+    #[test]
+    fn unique_pop_none_works() {
+        let mut vec = UniqueVec::<u32>::new();
+        assert_eq!(vec.pop(), None);
+    }
+
+    #[test]
+    fn unique_pop_some_works() {
+        let mut vec = UniqueVec::new();
+        vec.push(1);
+        vec.push(2);
+        assert_eq!(vec.pop(), Some(2));
+    }
+
+    #[test]
+    fn unique_insert_works() {
+        let mut vec = UniqueVec::new();
+        vec.insert(0, 10);
+        vec.insert(1, 10);
+        assert_eq!(vec.len(), 1);
+    }
+
+    #[test]
+    fn unique_remove_works() {
+        let mut vec = UniqueVec::new();
+        vec.push(1);
+        vec.push(2);
+        assert_eq!(vec.remove(0), 1);
+        assert_eq!(vec.len(), 1)
     }
 }
