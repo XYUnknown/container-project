f96a9b4c292fab36d2fc024026db2c8daf8204ef
Xueying Qin <qinxy1995@gmail.com>
2021-02-02 03:14:19 +0000

    Add deletion

--- rust_containers/src/binary_search_tree.rs
+++ rust_containers/src/binary_search_tree.rs
@@ -5,13 +5,14 @@ use std::vec::Vec;
 pub struct Node<T: Ord> {
     value: T,
     height: i64,
+    count: u32,
     left: Option<Box<Node<T>>>,
     right: Option<Box<Node<T>>>
 }
 
 impl <T: Ord> Node<T> {
     pub const fn new(v: T) -> Node<T> {
-        Node { value: v, height: 0, left: None, right: None }
+        Node { value: v, height: 0, count: 1, left: None, right: None }
     }
 }
 
@@ -92,14 +93,76 @@ pub fn insert<T: Ord> (v: T, mut parent: Option<Box<Node<T>>>) -> Option<Box<Nod
     match parent {
         Some(mut p) => {
             match p.value.cmp(&v) {
-                Ordering::Less | Ordering::Equal => p.right = insert(v, p.right.take()),
-                Ordering::Greater => p.left = insert(v, p.left.take())
+                Ordering::Less => p.right = insert(v, p.right.take()),
+                Ordering::Greater => p.left = insert(v, p.left.take()),
+                Ordering::Equal => p.count += 1,
             }
             update_height(&mut p);
             Some(balance(p))
         },
         None => {
             Some(Box::new(Node::new(v)))
+        }
+    }
+}
+
+fn find_left_min<T: Ord> (mut node : Box<Node<T>>, left: Box<Node<T>>) -> (Option<Box<Node<T>>>, Box<Node<T>>) {
+    let (new_left, min) = find_min(left);
+    node.left = new_left;
+    update_height(&mut node);
+    (Some(balance(node)), min)
+}
+
+fn find_min<T: Ord> (mut node: Box<Node<T>>) -> (Option<Box<Node<T>>>, Box<Node<T>>) {
+    match node.left.take() {
+        Some(left) => find_left_min(node, left),
+        None => (node.right.take(), node)
+    }
+}
+
+pub fn delete<T: Ord> (v: T, mut parent: Option<Box<Node<T>>>, size: &mut u32) -> Option<Box<Node<T>>> {
+    match parent {
+        Some(mut p) => {
+            match p.value.cmp(&v) {
+                Ordering::Less => p.right = delete(v, p.right.take(), size),
+                Ordering::Greater => p.left = delete(v, p.left.take(), size),
+                Ordering::Equal => {
+                    *size -= p.count; // update size
+                    match (p.left.take(), p.right.take()) {
+                        (None, None) => return None,
+                        (None, Some(r)) => return Some(r),
+                        (Some(l), None) => return Some(l),
+                        (Some(l), Some(r)) => {
+                            let (right_sub_tree, right_min) = find_min(r);
+                            p = right_min;
+                            p.left = Some(l);
+                            p.right = right_sub_tree;
+                            update_height(&mut p);
+                            return Some(balance(p));
+                        }
+                    }
+                }
+            }
+            update_height(&mut p);
+            Some(balance(p))
+        },
+        None => {
+            parent
+        }
+    }
+}
+
+pub fn contains<T: Ord> (v: &T, parent: &Option<Box<Node<T>>>) -> bool {
+    match parent {
+        Some(p) => {
+            match p.value.cmp(&v) {
+                Ordering::Less => return contains(v, &p.right),
+                Ordering::Greater => return contains(v, &p.left),
+                Ordering::Equal => return true,
+            }
+        },
+        None => {
+            false
         }
     }
 }
@@ -111,7 +174,11 @@ where
     match node {
         Some(n) => {
             in_order_to_vec(&n.left, vec);
-            vec.push(n.value);
+            let mut i = n.count;
+            while i > 0 {
+                vec.push(n.value);
+                i -= 1;
+            }
             in_order_to_vec(&n.right, vec);
         },
         None => {
@@ -119,7 +186,6 @@ where
         },
     }
 }
-
 
 pub struct BinarySearchTree<T: Ord> {
     root: Option<Box<Node<T>>>,
@@ -139,9 +205,22 @@ impl <T: Ord> BinarySearchTree<T> {
         self.len() == 0
     }
 
+    pub fn contains(&self, v: T) -> bool {
+        contains(&v, &self.root)
+    }
+
     pub fn insert(&mut self, v: T) {
         self.root = insert(v, self.root.take());
         self.size += 1;
+    }
+
+    pub fn delete(&mut self, v: T) {
+        self.root = delete(v, self.root.take(), &mut self.size);
+    }
+
+    pub fn clear(&mut self) {
+        self.root = None;
+        self.size = 0;
     }
 
     pub fn to_vec(&self) -> Vec<T> 
