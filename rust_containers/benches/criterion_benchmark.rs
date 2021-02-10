a701690421119947cfc61b258763a8a08a7eb432
Xueying Qin <qinxy1995@gmail.com>
2021-02-10 12:19:30 +0000

    Use larger data set for benchmarks

--- rust_containers/benches/criterion_benchmark.rs
+++ rust_containers/benches/criterion_benchmark.rs
@@ -1,11 +1,27 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::time::Duration;
+use std::mem::size_of;
+use std::collections::BTreeSet;
+use std::vec::Vec;
+use rand::{Rng,SeedableRng};
+use rand::rngs::StdRng;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 use rust_containers;
 use rust_containers::binary_search_tree::BinarySearchTree;
 use rust_containers::sorted_vector::SortedVec;
 use rust_containers::sorted_vector::SortedVecAlt;
-use std::collections::BTreeSet;
+use rust_containers::unique_vector::UniqueVec;
+
+fn gen_dataset() -> Vec<u32> { // avoid duplication
+    let size = 12 * 1024 * 1024; // 12 MB
+    let amount = size / size_of::<u32>();
+    let mut data: Vec<u32> = (1..amount as u32).collect(); //ensure no duplication
+    let mut rng = StdRng::seed_from_u64(222);
+    data.shuffle(&mut rng);
+    data
+}
 
 fn ten_sec_measurement() -> Criterion {
     let ten_seconds = Duration::new(10, 0);
@@ -17,50 +33,89 @@ fn fifteen_sec_measurement() -> Criterion {
     Criterion::default().measurement_time(fifteen_seconds)
 }
 
+fn long_measurement() -> Criterion {
+    let fifteen_seconds = Duration::new(1000, 0);
+    Criterion::default().measurement_time(fifteen_seconds)
+}
+
 fn bst_insertion(c: &mut Criterion) {
     let mut t = BinarySearchTree::new();
-    c.bench_function("bst insertion", |b| b.iter(|| for x in 0..10000 {
-        t.insert(x);
-    }));
+    let data = gen_dataset();
+    c.bench_function("bst insertion", |b| b.iter(||
+        for i in 0..data.len() {
+            let val = data.get(i);
+            match val {
+                Some(x) => t.insert(x),
+                None => break
+            }
+        }
+    ));
 }
 
 fn bst_contains(c: &mut Criterion) {
     let mut t = BinarySearchTree::new();
-    for x in 0..10000 {
-        t.insert(x);
+    let data = gen_dataset();
+    for i in 0..data.len() {
+        let val = data.get(i);
+        match val {
+            Some(x) => t.insert(*x),
+            None => break
+        }
     }
-    c.bench_function("bst contains", |b| b.iter(|| t.contains(&1)));
+    c.bench_function("bst contains", |b| b.iter(|| t.contains(&10000)));
 }
 
 fn bst_remove(c: &mut Criterion) {
     let mut t = BinarySearchTree::new();
-    for x in 0..10000 {
-        t.insert(x);
+    let data = gen_dataset();
+    for i in 0..data.len() {
+        let val = data.get(i);
+        match val {
+            Some(x) => t.insert(*x),
+            None => break
+        }
     }
-    c.bench_function("bst remove", |b| b.iter(|| t.remove(&5)));
+    c.bench_function("bst remove", |b| b.iter(|| t.remove(&10000)));
 }
 
 fn btreeset_insertion(c: &mut Criterion) {
     let mut t = BTreeSet::new();
-    c.bench_function("btreeset insertion", |b| b.iter(|| for x in 0..10000 {
-        t.insert(x);
-    })); 
+    let data = gen_dataset();
+    c.bench_function("btreeset insertion", |b| b.iter(|| 
+        for i in 0..data.len() {
+            let val = data.get(i);
+            match val {
+                Some(x) => {t.insert(x);},
+                None => break
+            }
+        }
+    )); 
 }
 
 fn btreeset_contains(c: &mut Criterion) {
     let mut t = BTreeSet::new();
-    for x in 0..10000 {
-        t.insert(x);
+    let data = gen_dataset();
+    for i in 0..data.len() {
+        let val = data.get(i);
+        match val {
+            Some(x) => {t.insert(x);},
+            None => break
+        }
     }
-    c.bench_function("btreeset contains", |b| b.iter(|| t.contains(&1)));
+    c.bench_function("btreeset contains", |b| b.iter(|| t.contains(&10000)));
 }
 
 fn btreeset_remove(c: &mut Criterion) {
     let mut t = BTreeSet::new();
-    for x in 0..10000 {
-        t.insert(x);
+    let data = gen_dataset();
+    for i in 0..data.len() {
+        let val = data.get(i);
+        match val {
+            Some(x) => {t.insert(x);},
+            None => break
+        }
     }
-    c.bench_function("btreeset remove", |b| b.iter(|| t.remove(&5)));
+    c.bench_function("btreeset remove", |b| b.iter(|| t.remove(&10000)));
 }
 
 fn sorted_vec_many_push_one_pop(c: &mut Criterion) {
@@ -83,11 +138,10 @@ fn sorted_vec_alt_many_push_one_pop(c: &mut Criterion) {
     }));
 }
 
-criterion_group!(five_sec_benches, bst_contains, bst_remove, btreeset_insertion, btreeset_contains, btreeset_remove);
 criterion_group! {
-    name = ten_sec_benches;
-    config = ten_sec_measurement();
-    targets = bst_insertion
+    name = tree_benches;
+    config = long_measurement();
+    targets = bst_insertion, bst_contains, bst_remove, btreeset_insertion, btreeset_contains, btreeset_remove
 }
 
 criterion_group! {
@@ -95,4 +149,4 @@ criterion_group! {
     config = fifteen_sec_measurement();
     targets = sorted_vec_many_push_one_pop, sorted_vec_alt_many_push_one_pop
 }
-criterion_main!(ten_sec_benches, five_sec_benches, vec_benches);
+criterion_main!(tree_benches, vec_benches);
