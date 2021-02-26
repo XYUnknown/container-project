2fe9e421a3a97644ad46c7e04702ec490942392c
Xueying Qin <qinxy1995@gmail.com>
2021-02-26 02:54:52 +0000

    Add trait object approach for getting a container according to specified properties

--- rust_containers/src/container.rs
+++ rust_containers/src/container.rs
@@ -1,5 +1,7 @@
 use std::any::{Any, TypeId};
+use std::vec::Vec;
 use crate::unique_vector::UniqueVec;
+use crate::sorted_vector::SortedVec;
 
 pub enum Property {
     Unique,
@@ -7,12 +9,12 @@ pub enum Property {
 }
 
 pub trait Container<T> {
-    fn push(&mut self, value: T);
-    fn pop(&mut self) -> Option<T>;
-    fn clear(&mut self);
-    fn len(&self) -> usize;
-    fn contains(&self, x: &T) -> bool;
-    fn is_empty(&self) -> bool;
+    fn c_push(&mut self, value: T);
+    fn c_pop(&mut self) -> Option<T>;
+    fn c_clear(&mut self);
+    fn c_len(&self) -> usize;
+    fn c_contains(&self, x: &T) -> bool;
+    fn c_is_empty(&self) -> bool;
 }
 
 pub trait Vector<T> : Container<T> {
@@ -23,6 +25,104 @@ pub fn type_of<T: ?Sized + Any>(_s: &T) -> TypeId {
     TypeId::of::<String>()
 }
 
+impl<T: PartialEq> Container<T> for Vec<T> {
+    fn c_push(&mut self, value: T) {
+        self.push(value);
+    }
+
+    fn c_pop(&mut self) -> Option<T> {
+        self.pop()
+    }
+
+    fn c_clear(&mut self) {
+        self.clear();
+    }
+
+    fn c_len(&self) -> usize {
+        self.len()
+    }
+
+    fn c_contains(&self, x: &T) -> bool {
+        self.contains(x)
+    }
+
+    fn c_is_empty(&self) -> bool {
+        self.is_empty()
+    }
+}
+
+impl<T: PartialEq> Container<T> for UniqueVec<T> {
+    fn c_push(&mut self, value: T) {
+        self.push(value);
+    }
+
+    fn c_pop(&mut self) -> Option<T> {
+        self.pop()
+    }
+
+    fn c_clear(&mut self) {
+        self.clear();
+    }
+
+    fn c_len(&self) -> usize {
+        self.len()
+    }
+
+    fn c_contains(&self, x: &T) -> bool {
+        self.contains(x)
+    }
+
+    fn c_is_empty(&self) -> bool {
+        self.is_empty()
+    }
+}
+
+impl<T: PartialEq> Container<T> for SortedVec<T> {
+    fn c_push(&mut self, value: T) {
+        self.push(value);
+    }
+
+    fn c_pop(&mut self) -> Option<T> {
+        self.pop()
+    }
+
+    fn c_clear(&mut self) {
+        self.clear();
+    }
+
+    fn c_len(&self) -> usize {
+        self.len()
+    }
+
+    fn c_contains(&self, x: &T) -> bool {
+        self.contains(x)
+    }
+
+    fn c_is_empty(&self) -> bool {
+        self.is_empty()
+    }
+}
+
+fn get_vec<T: 'static + Ord + PartialEq + Sized> (prop: Option<Property>) -> Box<dyn Container<T>> {
+    match prop {
+        Some(p) => {
+            match p {
+                Property::Unique => {
+                    let vec = Box::new(UniqueVec::<T>::new());
+                    vec
+                },
+                Property::Sorted => {
+                    let vec = Box::new(SortedVec::<T>::new());
+                    vec
+                }
+            }
+        },
+        None => {
+            let vec = Box::new(SortedVec::<T>::new());
+            vec
+        }
+    }
+}
 // experiment on macro
 // get a vector according to specific property(-ies)
 #[macro_export]
@@ -60,7 +160,7 @@ mod tests {
     use crate::unique_vector::UniqueVec;
     use crate::sorted_vector::SortedVec;
     use crate::unique_sorted_vector::UniqueSortedVec;
-    use crate::container::{Property, type_of};
+    use crate::container::{Property, type_of, Container, get_vec};
 
     // macro get_vec test
     #[test]
@@ -101,5 +201,15 @@ mod tests {
         let v1 = UniqueSortedVec::<u32>::new();
         assert!(v.is_empty());
         assert_eq!(type_of(&v), type_of(&v1));
+    }
+
+    #[test]
+    fn get_unique_container() {
+        let mut c = get_vec(Some(Property::Unique));
+        for x in 0..10000 {
+            c.c_push(x);
+            c.c_push(x);
+        }
+        assert_eq!(c.c_len(), 10000); // no duplication
     }
 }
