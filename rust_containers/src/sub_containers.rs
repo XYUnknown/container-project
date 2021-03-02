bacaa1eedb780887cf149d81a8b7bfc5fca9e59f
Xueying Qin <qinxy1995@gmail.com>
2021-03-02 21:20:28 +0000

    Add super trait approach

---
+++ rust_containers/src/sub_containers.rs
@@ -0,0 +1,75 @@
+use crate::container::{Property, type_of, Container};
+use std::vec::Vec;
+use crate::unique_vector::UniqueVec;
+use crate::sorted_vector::SortedVec;
+
+
+pub trait Vector<T> : Container<T> {
+    fn v_get(&mut self, index: usize) -> Option<&T>;
+    fn v_remove(&mut self, index: usize) -> T;
+    // issue: insert is not meaningful for sored vector
+    // fn v_insert(&mut self, index: usize, element: T);
+}
+
+impl<T: PartialEq> Vector<T> for Vec<T> {
+    fn v_get(&mut self, index: usize) -> Option<&T> {
+        self.get(index)
+    }
+
+    fn v_remove(&mut self, index: usize) -> T {
+        self.remove(index)
+    }
+}
+
+impl<T: PartialEq> Vector<T> for UniqueVec<T> {
+    fn v_get(&mut self, index: usize) -> Option<&T> {
+        self.get(index)
+    }
+
+    fn v_remove(&mut self, index: usize) -> T {
+        self.remove(index)
+    }
+}
+
+impl<T: PartialEq> Vector<T> for SortedVec<T> {
+    fn v_get(&mut self, index: usize) -> Option<&T> {
+        self.get(index)
+    }
+
+    fn v_remove(&mut self, index: usize) -> T {
+        self.remove(index)
+    }
+}
+
+fn get_vec<T: 'static + Ord + PartialEq + Sized> (prop: Option<Property>) -> Box<dyn Vector<T>> {
+    match prop {
+        Some(p) => {
+            match p {
+                Property::Unique => {
+                    let vec = Box::new(UniqueVec::<T>::new());
+                    vec
+                },
+                Property::Sorted => {
+                    let vec = Box::new(SortedVec::<T>::new());
+                    vec
+                }
+            }
+        },
+        None => {
+            let vec = Box::new(SortedVec::<T>::new());
+            vec
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::sub_containers::{get_vec};
+    #[test]
+    fn get_vec_container() {
+        let mut c = get_vec(None);
+        c.c_push(2);
+        assert_eq!(c.c_len(), 1);
+        assert_eq!(c.v_get(0), Some(&2));
+    }
+}
