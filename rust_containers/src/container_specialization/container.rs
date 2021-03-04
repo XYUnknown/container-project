12457f5b234d307694bb014a563602732a2aed5d
Xueying Qin <qinxy1995@gmail.com>
2021-03-04 02:58:07 +0000

    Add linked list implementation of the container

--- rust_containers/src/container_specialization/container.rs
+++ rust_containers/src/container_specialization/container.rs
@@ -1,6 +1,10 @@
 use std::vec::Vec;
-use std::ops::Deref;
+use std::collections::LinkedList;
+use std::ops::{Deref, DerefMut};
 use std::marker::PhantomData;
+use std::cmp::Ordering;
+use std::collections::linked_list::Cursor;
+use std::collections::linked_list::CursorMut;
 
 // Properties as marker traits
 pub trait Unique {}
@@ -36,12 +40,37 @@ impl<T: PartialEq, P: ?Sized> VecWrapper<T, P> {
     }
 }
 
+pub struct LinkedListWrapper<T, P: ?Sized> {
+    ll: LinkedList<T>,
+    property: PhantomData<P>
+}
+
+impl<T: PartialEq, P: ?Sized> LinkedListWrapper<T, P> {
+    pub const fn new() -> LinkedListWrapper<T, P> {
+        LinkedListWrapper { ll: LinkedList::new(), property: PhantomData }
+    }
+}
+
 // just for testing purposes
 impl<T, P: ?Sized> Deref for VecWrapper<T, P> {
     type Target = Vec<T>;
 
     fn deref(&self) -> &Self::Target {
         &self.v
+    }
+}
+
+impl<T, P: ?Sized> Deref for LinkedListWrapper<T, P> {
+    type Target = LinkedList<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.ll
+    }
+}
+
+impl<T, P: ?Sized> DerefMut for LinkedListWrapper<T, P> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.ll
     }
 }
 
@@ -103,6 +132,94 @@ impl<T: PartialEq + Ord> Container<T, dyn UniqueSorted> for VecWrapper<T, dyn Un
     }
 }
 
+impl<T: PartialEq, P: ?Sized> Container<T, P> for LinkedListWrapper<T, P> {
+    default fn push(&mut self, value: T) {
+        self.ll.push_back(value);
+    }
+
+    default fn pop(&mut self) -> Option<T> {
+        self.ll.pop_back()
+    }
+
+    default fn clear(&mut self) {
+        self.ll.clear();
+    }
+
+    default fn len(&self) -> usize {
+        self.ll.len()
+    }
+
+    default fn contains(&self, x: &T) -> bool {
+        self.ll.contains(x)
+    }
+
+    default fn is_empty(&self) -> bool {
+        self.ll.is_empty()
+    }
+
+    default fn first(&mut self) -> Option<&T> {
+        self.ll.front()
+    }
+
+    default fn last(&mut self) -> Option<&T> {
+        self.ll.back()
+    }
+}
+
+impl<T: PartialEq> Container<T, dyn Unique> for LinkedListWrapper<T, dyn Unique> {
+    fn push(&mut self, value: T) {
+        if !self.ll.contains(&value) {
+            self.ll.push_front(value);
+        }
+    }
+}
+
+impl<T: Ord + PartialEq> Container<T, dyn Sorted> for LinkedListWrapper<T, dyn Sorted> {
+    fn push(&mut self, elt: T) {
+        let mut c = self.cursor_front_mut();
+        loop {
+            match c.current() {
+                Some(x) => {
+                    match &elt.cmp(x) {
+                        Ordering::Less | Ordering::Equal => {
+                            c.insert_before(elt);
+                            break;
+                        },
+                        Ordering::Greater => c.move_next()
+                    }
+                },
+                None => { // empty list
+                    c.insert_before(elt);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+impl<T: Ord + PartialEq> Container<T, dyn UniqueSorted> for LinkedListWrapper<T, dyn UniqueSorted> {
+    fn push(&mut self, elt: T) {
+        let mut c = self.cursor_front_mut();
+        loop {
+            match c.current() {
+                Some(x) => {
+                    match &elt.cmp(x) {
+                        Ordering::Less => {
+                            c.insert_before(elt);
+                            break;
+                        },
+                        Ordering::Equal => break,
+                        Ordering::Greater => c.move_next()
+                    }
+                },
+                None => { // empty list
+                    c.insert_before(elt);
+                    break;
+                }
+            }
+        }
+    }
+}
 //impl<T: PartialEq, P: ?Sized> Vector<T, P> for VecWrapper<T, P> {
 //
 //}
@@ -112,9 +229,14 @@ fn get_vec<T: 'static + PartialEq + Ord + Sized, P: 'static + ?Sized> () -> Box<
     vec
 }
 
+fn get_list<T: 'static + PartialEq + Ord + Sized, P: 'static + ?Sized> () -> Box<dyn Container<T, P>> {
+    let ll = Box::new(LinkedListWrapper::<T, P>::new());
+    ll
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::container_specialization::container::{Container, Unique, Sorted, UniqueSorted, VecWrapper, get_vec };
+    use crate::container_specialization::container::{Container, Unique, Sorted, UniqueSorted, VecWrapper, LinkedListWrapper, get_vec, get_list };
 
     #[test]
     fn vec_specialize_works() {
@@ -223,6 +345,128 @@ mod tests {
         }
         assert_eq!(c.len(), 100); // no duplication
     }
+
+    #[test]
+    fn ll_specialize_works() {
+        let mut c = LinkedListWrapper::<u32, ()>::new();
+        assert!(c.is_empty());
+        for x in 0..100 {
+            c.push(x);
+            c.push(x);
+        }
+        assert_eq!(c.len(), 200); // duplication allowed
+    }
+
+    #[test]
+    fn ll_specialize_unique_works() {
+        let mut c = LinkedListWrapper::<u32, dyn Unique>::new();
+        assert!(c.is_empty());
+        for x in 0..100 {
+            c.push(x);
+            c.push(x);
+        }
+        assert_eq!(c.len(), 100); // no duplication
+    }
+
+    #[test]
+    fn ll_specialize_sorted_works() {
+        let mut c = LinkedListWrapper::<_, dyn Sorted>::new();
+        assert!(c.is_empty());
+        assert!(c.is_empty());
+        for x in 0..5 {
+            c.push(4 - x);
+        }
+        // increasing
+        assert_eq!(c.pop(), Some(4));
+        assert_eq!(c.pop(), Some(3));
+        assert_eq!(c.pop(), Some(2));
+        assert_eq!(c.pop(), Some(1));
+        assert_eq!(c.pop(), Some(0));
+        assert_eq!(c.pop(), None);
+    }
+
+    #[test]
+    fn ll_specialize_unique_sorted_works() {
+        let mut c = LinkedListWrapper::<_, dyn UniqueSorted>::new();
+        assert!(c.is_empty());
+        assert!(c.is_empty());
+        for x in 0..5 {
+            c.push(4 - x);
+        }
+        // increasing
+        assert_eq!(c.pop(), Some(4));
+        assert_eq!(c.pop(), Some(3));
+        assert_eq!(c.pop(), Some(2));
+        assert_eq!(c.pop(), Some(1));
+        assert_eq!(c.pop(), Some(0));
+        assert_eq!(c.pop(), None);
+        for x in 0..100 {
+            c.push(x);
+            c.push(x);
+        }
+        assert_eq!(c.len(), 100); // no duplication
+    }
+
+    #[test]
+    fn get_list_specialize_works() {
+        let mut c = get_list::<u32, ()>();
+        assert!(c.is_empty());
+        for x in 0..100 {
+            c.push(x);
+            c.push(x);
+        }
+        assert_eq!(c.len(), 200); // duplication allowed
+    }
+
+    #[test]
+    fn get_list_specialize_unique_works() {
+        let mut c = get_list::<u32, dyn Unique>();
+        assert!(c.is_empty());
+        for x in 0..100 {
+            c.push(x);
+            c.push(x);
+        }
+        assert_eq!(c.len(), 100); // no duplication
+    }
+
+    #[test]
+    fn get_list_specialize_sorted_works() {
+        let mut c = get_list::<u32, dyn Sorted>();
+        assert!(c.is_empty());
+        for x in 0..5 {
+            c.push(4 - x);
+        }
+        // increasing
+        assert_eq!(c.pop(), Some(4));
+        assert_eq!(c.pop(), Some(3));
+        assert_eq!(c.pop(), Some(2));
+        assert_eq!(c.pop(), Some(1));
+        assert_eq!(c.pop(), Some(0));
+        assert_eq!(c.pop(), None);
+    }
+
+    #[test]
+    fn get_list_specialize_unique_sorted_works() {
+        let mut c = get_list::<_, dyn UniqueSorted>();
+        assert!(c.is_empty());
+        for x in 0..5 {
+            c.push(4 - x);
+        }
+        // increasing
+        assert_eq!(c.pop(), Some(4));
+        assert_eq!(c.pop(), Some(3));
+        assert_eq!(c.pop(), Some(2));
+        assert_eq!(c.pop(), Some(1));
+        assert_eq!(c.pop(), Some(0));
+        assert_eq!(c.pop(), None);
+        for x in 0..100 {
+            c.push(x);
+            c.push(x);
+        }
+        assert_eq!(c.len(), 100); // no duplication
+    }
+
+
 }
 
 /* INVALID
