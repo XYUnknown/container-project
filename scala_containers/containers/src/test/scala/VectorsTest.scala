20fbadcd3915a70f7423cf50b45cd623a5b66c1e
Xueying Qin <qinxy1995@gmail.com>
2021-03-30 02:57:25 +0100

    Add the basic scala implementation

---
+++ scala_containers/containers/src/test/scala/VectorsTest.scala
@@ -0,0 +1,51 @@
+import vector.Vec
+import vector.UniqueVec
+import vector.SortedVec
+import vector.UniqueSortedVec
+import scala.collection.immutable.Vector
+import org.scalatest.funsuite.AnyFunSuite
+    
+class VecTest extends AnyFunSuite {
+    test("Vec.new") {
+        val vec = new Vec[Int](v = Vector.empty)
+        assert(vec.size == 0)
+    }
+
+    test("Vec.appended") {
+        var vec = new Vec[Int](v = Vector.empty)
+        for ( a <- 0 until 10){
+            vec = vec.appended(a)
+            vec = vec.appended(a)
+        }
+        assert(vec.size == 20)
+    }
+
+    test("UniqueVec.appended") {
+        var vec = new UniqueVec[Int](v = Vector.empty)
+        for ( a <- 0 until 10){
+            vec = vec.appended(a)
+            vec = vec.appended(a)
+        }
+        assert(vec.size == 10)
+    }
+
+    test("SortedVec.appended") {
+        var vec = new SortedVec[Int](v = Vector.empty)
+        for ( a <- 0 until 10){
+            vec = vec.appended(9 - a)
+            vec = vec.appended(9 - a)
+        }
+        assert(vec.size == 20)
+        assert(vec.isSorted)
+    }
+
+    test("UniqueSortedVec.appended") {
+        var vec = new UniqueSortedVec[Int](v = Vector.empty)
+        for ( a <- 0 until 10){
+            vec = vec.appended(9 - a)
+            vec = vec.appended(9 - a)
+        }
+        assert(vec.size == 10)
+        assert(vec.isSorted)
+    }
+}
