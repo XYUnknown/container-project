4d071cd45220b825fe5d64aa6998388e4d312fd0
Xueying Qin <qinxy1995@gmail.com>
2021-04-08 15:00:12 +0100

    Add property composition

--- cpp_containers/containers.cpp
+++ cpp_containers/containers.cpp
@@ -4,6 +4,9 @@
 class Unique {};
 class Sorted {};
 
+template <class P1, class P2>
+struct And;
+
 // We can try making a container where we can sepecify what concrete container type and 
 // semantic properties 
 template<class T, class C, class P>
@@ -17,53 +20,92 @@ struct Container<T, std::vector<T>, void> : public std::vector<T> {
 };
 
 template <class T>
-struct Container<T, std::vector<T>, Unique> : Container<T, std::vector<T>, void> {
+struct Container<T, std::vector<T>, Unique> : public virtual Container<T, std::vector<T>, void> {
     void push_back(T t) {
         if (!this->contains(t)) {
             Container<T, std::vector<T>, void>::push_back(t);
         }
     }
+
+    auto insert(typename std::vector<T>::iterator pos, T t) {
+        if (!this->contains(t)) {
+            Container<T, std::vector<T>, void>::insert(pos, t);
+        }
+    }
 };
 
 template <class T>
-struct Container<T, std::vector<T>, Sorted> : Container<T, std::vector<T>, void> {
+struct Container<T, std::vector<T>, Sorted> : public virtual Container<T, std::vector<T>, void> {
     void push_back(T t) {
-        auto pos = std::lower_bound(std::vector<T>::begin(), std::vector<T>::end(), t);
+        auto pos = std::lower_bound(this->begin(), this->end(), t);
         Container<T, std::vector<T>, void>::insert(pos, t);
     }
+
+    auto insert(typename std::vector<T>::iterator pos, T t) {
+        if (this->at(pos-this->begin()) >= t) {
+            auto pos_i = std::lower_bound(this->begin(), pos, t);
+            Container<T, std::vector<T>, void>::insert(pos_i, t);
+        } else {
+            auto pos_i = std::lower_bound(pos, this->end(), t);
+            Container<T, std::vector<T>, void>::insert(pos_i, t);
+        }
+    }
 };
+
+template <class T>
+struct Container<T, std::vector<T>, And<Sorted, Unique>> : Container<T, std::vector<T>, Sorted>, Container<T, std::vector<T>, Unique> {
+    void push_back(T t) {
+        auto pos = std::lower_bound(this->begin(), this->end(), t);
+        Container<T, std::vector<T>, Unique>::insert(pos, t);
+    }
+
+    auto insert(typename std::vector<T>::iterator pos, T t) {
+        if (this->at(pos-this->begin()) >= t) {
+            auto pos_i = std::lower_bound(this->begin(), pos, t);
+            Container<T, std::vector<T>, Unique>::insert(pos_i, t);
+        } else {
+            auto pos_i = std::lower_bound(pos, this->end(), t);
+            Container<T, std::vector<T>, Unique>::insert(pos_i, t);
+        }
+    }
+};
+
+void print_vector(std::vector<int> v) {
+    std::cout << "Size: " << v.size() << std::endl;
+    for (auto it=v.begin(); it<v.end(); it++)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+    std::cout << '\n';
+}
 
 int main() {
     Container<int, std::vector<int>, void> c1;
     c1.push_back(1);
     c1.push_back(1);
     std::cout << "Container for default vector" << std::endl;
-    std::cout << "Size: " << c1.size() << std::endl;
-    for (auto it=c1.begin(); it<c1.end(); it++)
-        std::cout << ' ' << *it;
-    std::cout << '\n';
-    std::cout << '\n';
+    print_vector(c1);
 
     Container<int, std::vector<int>, Unique> c2;
     c2.push_back(1);
     c2.push_back(1);
+    c2.insert(c2.begin(), 1);
     std::cout << "Container for unique vector" << std::endl;
-    std::cout << "Size: " << c2.size() << std::endl;
-    for (auto it=c2.begin(); it<c2.end(); it++)
-        std::cout << ' ' << *it;
-    std::cout << '\n';
-    std::cout << '\n';
+    print_vector(c2);
 
     Container<int, std::vector<int>, Sorted> c3;
     c3.push_back(3);
     c3.push_back(1);
     c3.push_back(3);
-    std::cout << "Container for unique vector" << std::endl;
-    std::cout << "Size: " << c3.size() << std::endl;
-    for (auto it=c3.begin(); it<c3.end(); it++)
-        std::cout << ' ' << *it;
-    std::cout << '\n';
-    std::cout << '\n';
+    c3.insert(c3.begin(), 4);
+    std::cout << "Container for sorted vector" << std::endl;
+    print_vector(c3);
+
+    Container<int, std::vector<int>, And<Sorted, Unique>> c4;
+    c4.push_back(3);
+    c4.push_back(1);
+    c4.push_back(3);
+    std::cout << "Container for unique sorted vector" << std::endl;
+    print_vector(c4);
 
     return 0;
 }
