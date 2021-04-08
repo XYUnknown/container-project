9fbf1f217e440942793ccf2784cad3b5f5bf34e0
Xueying Qin <qinxy1995@gmail.com>
2021-04-08 02:32:09 +0100

    Update containers.cpp

--- cpp_containers/containers.cpp
+++ cpp_containers/containers.cpp
@@ -1,100 +1,69 @@
 #include <iostream>
-#include <variant>
 #include <vector>
-using namespace std;
 
 class Unique {};
 class Sorted {};
 
-/*template <typename T, typename P>
-class Container {
-    public:
-    virtual void push(T);
-    virtual T pop();
-    virtual void clear();
-    virtual size_t size();
-    virtual bool contains(T);
-    virtual bool is_empty();
+template<class T, class P>
+struct Vec;
 
-    Container(){}
-    virtual ~Container() {}
-};*/
-
-template<typename T>
-class Vec /*: public Container<T, P>*/ {
-    private:
-    vector<T> v;
-
-    public:
-    Vec() /*: Container<T, P> ()*/ {}
-    ~Vec() {}
-    template<typename P> void push(T value);
-    T pop();
-    void clear();
-    size_t size();
-    bool is_empty();
-    bool contains(T value);
+template <class T>
+struct Vec<T, void> : public std::vector<T> {
+    bool contains(T t) {
+        return find(std::vector<T>::begin(), std::vector<T>::end(), t) != std::vector<T>::end();
+    }
 };
 
-template<typename T>
-template<typename P> void Vec<T>::push(T value) {
-    v.push_back(value);
-}
-
-template<typename T>
-T Vec<T>::pop() {
-    T value = v.back();
-    v.pop_back();
-    return value;
-}
-
-template<typename T>
-void Vec<T>::clear() {
-    v.clear();
-}
-
-template<typename T>
-size_t Vec<T>::size() {
-    return v.size();
-}
-
-template<typename T>
-bool Vec<T>::is_empty() {
-    return v.empty();
-}
-
-template<typename T>
-bool Vec<T>::contains(T value) {
-    return find(v.begin(), v.end(), value) != v.end();
-}
-
-// This is valid
-template<>
-template<> void Vec<int>::push<Unique>(int value) {
-    if (!this->contains(value)) {
-        v.push_back(value);
+template <class T>
+struct Vec<T, Unique> : Vec<T, void> {
+    void push_back(T t) {
+        if (!this->contains(t)) {
+            Vec<T, void>::push_back(t);
+        }
     }
-}
+};
 
-// This is invalid
-/*
-template<typename T>
-template<> void Vec<T>::push<Unique>(T value) {
-    if (!this->contains(value)) {
-        v.push_back(value);
+template <class T>
+struct Vec<T, Sorted> : Vec<T, void> {
+    void push_back(T t) {
+        auto pos = std::lower_bound(std::vector<T>::begin(), std::vector<T>::end(), t);
+        std::vector<T>::insert(pos, t);
     }
-}*/
+};
 
 int main() {
-    Vec<int> v1;
-    v1.push<monostate>(1);
-    v1.push<monostate>(1);
+    Vec<int, void> v1;
+    v1.push_back(1);
+    v1.push_back(1);
 
-    Vec<int> v2;
-    v2.push<Unique>(1);
-    v2.push<Unique>(1);
+    Vec<int, Unique> v2;
+    v2.push_back(1);
+    v2.push_back(1);
 
-    cout << v1.size() << endl; //2
-    cout << v2.size() << endl; //1
+    Vec<int, Sorted> v3;
+    v3.push_back(3);
+    v3.push_back(1);
+    v3.push_back(3);
+
+    std::cout << "Default Vec" << std::endl;
+    std::cout << "Size: " << v1.size() << std::endl;
+    for (auto it=v1.begin(); it<v1.end(); it++)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+    std::cout << '\n';
+
+    std::cout << "Unique Vec" << std::endl;
+    std::cout << "Size: " << v2.size() << std::endl;
+    for (auto it=v2.begin(); it<v2.end(); it++)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+    std::cout << '\n';
+
+    std::cout << "Sorted Vec" << std::endl;
+    std::cout << "Size: " << v3.size() << std::endl;
+    for (auto it=v3.begin(); it<v3.end(); it++)
+        std::cout << ' ' << *it;
+    std::cout << '\n';
+
     return 0;
 }
