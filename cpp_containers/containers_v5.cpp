8c4cc9841ba4683897fd19ba85d613c24a900a18
Xueying Qin <qinxy1995@gmail.com>
2021-05-04 15:28:38 +0100

    Add insert(val)

--- cpp_containers/containers_v5.cpp
+++ cpp_containers/containers_v5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <list>
 #include <set>
@@ -55,8 +56,24 @@ struct Container<T, C> : private C<T> {
 
     template <class Q = T>
         requires requires (C<Q>& c, typename C<Q>::iterator pos, Q val) { { c.insert(pos, val) } -> std::same_as<typename C<Q>::iterator>; }
-    typename C<Q>::iterator insert(typename C<Q>::iterator pos, T t) {
+    typename C<Q>::iterator insert(typename C<Q>::iterator pos, Q t) {
         return C<Q>::insert(pos, t);
+    }
+
+    // if a container has insert(val)
+    template <class Q = T>
+        requires requires (C<Q>& c, Q val) { { c.insert(val) } -> std::same_as<std::pair<typename C<Q>::iterator, bool>>; }
+    std::pair<typename C<Q>::iterator, bool> insert(Q t) {
+        return C<Q>::insert(t);
+    }
+
+    // if a container does not have insert val
+    template <class Q = T>
+        requires (!requires (C<Q>& c, Q val) { { c.insert(val) } -> std::same_as<std::pair<typename C<Q>::iterator, bool>>; })
+    std::pair<typename C<Q>::iterator, bool> insert(Q t) {
+        size_t old_size = this->size();
+        C<Q>::push_back(t);
+        return std::pair(this->end(), (old_size < this->size()));
     }
 
     template <class Q = T>
@@ -85,14 +102,20 @@ struct Container<T, C> : private C<T> {
 };
 
 template<class T, template<typename...> class C, class ...Ps>
-struct Container<T, C, Unique, Ps...> : Container<T, C, Ps...> {
-    auto push_back(T t) {
+struct Container<T, C, Unique, Ps...> : private Container<T, C, Ps...> {
+    using Container<T, C, Ps...>::begin;
+    using Container<T, C, Ps...>::end;
+    using Container<T, C, Ps...>::size;
+    using Container<T, C, Ps...>::empty;
+    using Container<T, C, Ps...>::contains;
+    using Container<T, C, Ps...>::print;
+    void push_back(T t) {
         if (!this->contains(t)) {
             Container<T, C, Ps...>::push_back(t);
         }
     }
 
-    auto push_front(T t) {
+    void push_front(T t) {
         if (!this->contains(t)) {
             Container<T, C, Ps...>::push_front(t);
         }
@@ -103,35 +126,34 @@ struct Container<T, C, Unique, Ps...> : Container<T, C, Ps...> {
             Container<T, C, Ps...>::insert(pos, t);
         }
     }
+
+    auto insert(T t) {
+        if (!this->contains(t)) {
+            Container<T, C, Ps...>::insert(t);
+        }
+    }
 };
 
 template<class T, template<typename...> class C, class ...Ps>
-struct Container<T, C, Sorted, Ps...> : Container<T, C, Ps...> {
-    auto push_back(T t) {
-        this->insert(this->end(), t);
-    }
+struct Container<T, C, Sorted, Ps...> : private Container<T, C, Ps...> {
+    using Container<T, C, Ps...>::begin;
+    using Container<T, C, Ps...>::end;
+    using Container<T, C, Ps...>::size;
+    using Container<T, C, Ps...>::empty;
+    using Container<T, C, Ps...>::contains;
+    using Container<T, C, Ps...>::print;
 
-    auto push_front(T t) {
-        if constexpr (std::is_same<C<T>, std::list<T>>::value) {
-            this->insert(this->begin(), t);
-        } else {
-            static_assert(dependent_false<T>::value, "push_front is only defined for list");
-        }
-    }
-
-    auto insert(typename C<T>::iterator pos, T t) {
-        auto pos_i = std::lower_bound(this->begin(), pos, t);
-        if (pos_i == pos) {
-            pos_i = std::lower_bound(pos, this->end(), t);
-            Container<T, C, Ps...>::insert(pos_i, t);
-        } else {
-            Container<T, C, Ps...>::insert(pos_i, t);
-        }
+    // for a sorted container, it is not meaningful to choose the position for an element
+    // to be inserted in.
+    auto insert(T t) {
+        auto pos = std::lower_bound(this->begin(), this->end(), t);
+        Container<T, C, Ps...>::insert(pos, t);
     }
 };
 
 int main() {
     Container<int, std::vector> v1;
+    v1.insert(6);
     v1.push_back(3);
     v1.insert(v1.begin(), 5);
     v1.insert(v1.end(), 7);
@@ -175,56 +197,56 @@ int main() {
     l2.print();
 
     Container<int, std::vector, Sorted> v3;
-    v3.push_back(6);
-    v3.push_back(1);
-    v3.insert(v3.begin(), 1);
+    v3.insert(6);
+    v3.insert(1);
+    v3.insert(6);
+    v3.insert(5);
     std::cout << "Container for sorted vector" << std::endl;
     v3.print();
 
     Container<int, std::list, Sorted> l3;
-    l3.push_back(6);
-    l3.push_back(1);
-    l3.push_front(4);
-    l3.insert(l3.begin(), 1);
+    l3.insert(6);
+    l3.insert(4);
+    l3.insert(1);
+    l3.insert(4);
     std::cout << "Container for sorted list" << std::endl;
     l3.print();
 
     Container<int, std::vector, Sorted, Unique> v4;
-    v4.push_back(6);
-    v4.push_back(1);
-    v4.insert(v4.begin(), 1);
+    v4.insert(6);
+    v4.insert(1);
+    v4.insert(1);
     std::cout << "Container for sorted unique vector" << std::endl;
     v4.print();
 
     Container<int, std::list, Sorted, Unique> l4;
-    l4.push_back(6);
-    l4.push_back(1);
-    l4.push_back(1);
-    l4.push_front(4);
-    l4.insert(l4.begin(), 1);
+    l4.insert(6);
+    l4.insert(1);
+    l4.insert(1);
     std::cout << "Container for sorted unique list" << std::endl;
     l4.print();
 
     Container<int, std::vector, Unique, Sorted> v5;
-    v5.push_back(6);
-    v5.push_back(1);
-    v5.insert(v5.begin(), 1);
+    v5.insert(6);
+    v5.insert(1);
+    v5.insert(1);
     std::cout << "Container for unique sorted vector" << std::endl;
     v5.print();
 
     Container<int, std::list, Unique, Sorted> l5;
-    l5.push_back(6);
-    l5.push_back(1);
-    l5.push_back(1);
-    l5.push_front(4);
-    l5.insert(l5.begin(), 1);
+    l5.insert(6);
+    l5.insert(1);
+    l5.insert(1);
     std::cout << "Container for unique sorted list" << std::endl;
     l5.print();
 
     Container<int, std::set> s;
     s.size();
     //s.push_front(6);
-    //s.insert(6);
+    s.insert(6);
+    s.insert(s.begin(), 7);
+    std::cout << "Container for set" << std::endl;
+    s.print();
 
     return 0;
 }
