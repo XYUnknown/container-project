075cc211c76e2244d9e0bdbf99a6479a69784907
Xueying Qin <qinxy1995@gmail.com>
2021-06-08 17:55:42 +0100

    Add 1GB data test fixture for lookup

--- cpp_containers/benchmark.cc
+++ cpp_containers/benchmark.cc
@@ -126,6 +126,30 @@ public:
     }
 };
 
+template<template<typename...> class C, class ...Ps>
+class LookUp_1000_1GB: public ::benchmark::Fixture {
+public:
+    // size_t --> 8 bytes
+    std::size_t size = 128*1024*1024; // 1GB data
+    Container<std::size_t, C, Ps...> c;
+
+    std::size_t lookup_size = 1000; // perform 1000 lookups
+    std::vector<std::size_t> lookups;
+
+    void SetUp(const ::benchmark::State& st) {
+        lookups = Generate(lookup_size);
+        Generate(c, size);
+        if constexpr (has_property<SortedOnAccess, Ps...>()) {
+            c.sort(); // for sorted vectors and sorted lists
+        }
+    }
+
+    void TearDown(const ::benchmark::State&) {
+        lookups.clear();
+        c.clear();
+    }
+};
+
 /* 1KB 1000 lookups*/
 BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_1KB, SortedOnAccessVec_1KB_1000LookUp, std::vector, SortedOnAccess)(benchmark::State& state) {
     volatile std::size_t result;
@@ -389,6 +413,72 @@ BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_100MB, SortedOnAccessList_100MB_1000Look
     }
 }
 BENCHMARK_REGISTER_F(LookUp_1000_100MB, SortedOnAccessList_100MB_1000LookUp)->Unit(benchmark::kMillisecond);
+
+/* 1GB 1000 lookups*/
+BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_1GB, SortedOnAccessVec_1GB_1000LookUp, std::vector, SortedOnAccess)(benchmark::State& state) {
+    volatile std::size_t result;
+    while (state.KeepRunning()) {
+        for (std::size_t item : lookups) {
+            auto it = c.find(item);
+            if (it != c.end()) {
+                result = *it;
+            }
+        }
+    }
+}
+BENCHMARK_REGISTER_F(LookUp_1000_1GB, SortedOnAccessVec_1GB_1000LookUp)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_1GB, BST_1GB_1000LookUp, TreeWrapper)(benchmark::State& state) {
+    volatile std::size_t result;
+    while (state.KeepRunning()) {
+        for (std::size_t item : lookups) {
+            auto it = c.find(item);
+            if (it != c.end()) {
+                result = *it;
+            }
+        }
+    }
+}
+BENCHMARK_REGISTER_F(LookUp_1000_1GB, BST_1GB_1000LookUp)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_1GB, HashSet_1GB_1000LookUp, HashSetWrapper)(benchmark::State& state) {
+    volatile std::size_t result;
+    while (state.KeepRunning()) {
+        for (std::size_t item : lookups) {
+            auto it = c.find(item);
+            if (it != c.end()) {
+                result = *it;
+            }
+        }
+    }
+}
+BENCHMARK_REGISTER_F(LookUp_1000_1GB, HashSet_1GB_1000LookUp)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_1GB, TreeSet_1GB_1000LookUp, TreeSetWrapper)(benchmark::State& state) {
+    volatile std::size_t result;
+    while (state.KeepRunning()) {
+        for (std::size_t item : lookups) {
+            auto it = c.find(item);
+            if (it != c.end()) {
+                result = *it;
+            }
+        }
+    }
+}
+BENCHMARK_REGISTER_F(LookUp_1000_1GB, TreeSet_1GB_1000LookUp)->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE_DEFINE_F(LookUp_1000_1GB, SortedOnAccessList_1GB_1000LookUp, std::list, SortedOnAccess)(benchmark::State& state) {
+    volatile std::size_t result;
+    while (state.KeepRunning()) {
+        for (std::size_t item : lookups) {
+            auto it = c.find(item);
+            if (it != c.end()) {
+                result = *it;
+            }
+        }
+    }
+}
+BENCHMARK_REGISTER_F(LookUp_1000_1GB, SortedOnAccessList_1GB_1000LookUp)->Unit(benchmark::kMillisecond);
 
 /** Insertion benchmarks */
 class InsertionFixture_1KB : public ::benchmark::Fixture {
