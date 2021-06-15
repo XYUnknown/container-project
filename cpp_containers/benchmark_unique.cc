0d124b760df3901fd1935283b8ab61afcebb11e4
Xueying Qin <qinxy1995@gmail.com>
2021-06-15 22:28:22 +0100

    add benchmarks for removing duplication

---
+++ cpp_containers/benchmark_unique.cc
@@ -0,0 +1,134 @@
+#include <benchmark/benchmark.h>
+#include <algorithm>
+#include <cstdint>
+#include <random>
+#include <chrono>
+#include <vector>
+#include <set>
+
+#include "containers_v6.hpp"
+
+/**
+ * Given a vector, the performance of different implementation of removing its duplicated items
+ */
+
+/** Data generation */
+std::vector<std::size_t> Generate(std::size_t size) {
+    std::minstd_rand generator;
+    generator.seed(size);
+    std::vector<std::size_t> v(size);
+    std::generate(v.begin(), v.end(), generator);
+    return v;
+}
+
+template<class T, template<typename...> class C, class ...Ps>
+void Generate(Container<T, C, Ps...>& c, std::size_t size) {
+    std::minstd_rand generator;
+    generator.seed(size);
+    for (std::size_t i=0; i < size; i++)
+        c.insert((std::size_t)generator());
+}
+
+class UniqueFixture: public ::benchmark::Fixture {
+public:
+    // size_t --> 8 bytes
+    std::vector<std::size_t> data;
+    void SetUp(const ::benchmark::State& st) {
+        data = Generate(st.range(0));
+    }
+
+    void TearDown(const ::benchmark::State&) {
+        data.clear();
+    }
+};
+
+// std sort and unique
+BENCHMARK_DEFINE_F(UniqueFixture, STD_Sort_Unique)(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        std::sort(data.begin(), data.end());
+        data.erase(std::unique(data.begin(), data.end()), data.end());
+    }
+}
+BENCHMARK_REGISTER_F(UniqueFixture, STD_Sort_Unique)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({128}) // 1KB
+    ->Args({10*128}) // 10KB
+    ->Args({100*128}) // 100KB
+    ->Args({128*1024}) // 1MB
+    ->Args({10*128*1024}) // 10MB
+    ->Args({100*128*1024}) // 100MB
+    ->Args({128*1024*1024}); // 1GB
+
+// using a hashset
+BENCHMARK_DEFINE_F(UniqueFixture, HashSet_Remove_Dup)(benchmark::State& state) {
+    Container<std::size_t, HashSetWrapper> c;
+    while (state.KeepRunning()) {
+        for (size_t e : data)
+            c.insert(e);
+    }
+}
+BENCHMARK_REGISTER_F(UniqueFixture, HashSet_Remove_Dup)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({128}) // 1KB
+    ->Args({10*128}) // 10KB
+    ->Args({100*128}) // 100KB
+    ->Args({128*1024}) // 1MB
+    ->Args({10*128*1024}) // 10MB
+    ->Args({100*128*1024}) // 100MB
+    ->Args({128*1024*1024}); // 1GB
+
+// using a treeset
+BENCHMARK_DEFINE_F(UniqueFixture, TreeSet_Remove_Dup)(benchmark::State& state) {
+    Container<std::size_t, TreeSetWrapper> c;
+    while (state.KeepRunning()) {
+        for (size_t e : data)
+            c.insert(e);
+    }
+}
+BENCHMARK_REGISTER_F(UniqueFixture, TreeSet_Remove_Dup)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({128}) // 1KB
+    ->Args({10*128}) // 10KB
+    ->Args({100*128}) // 100KB
+    ->Args({128*1024}) // 1MB
+    ->Args({10*128*1024}) // 10MB
+    ->Args({100*128*1024}) // 100MB
+    ->Args({128*1024*1024}); // 1GB
+
+// using a unique vector
+BENCHMARK_DEFINE_F(UniqueFixture, Vector_Remove_Dup)(benchmark::State& state) {
+    Container<std::size_t, std::vector, Unique> c;
+    while (state.KeepRunning()) {
+        for (size_t e : data)
+            c.insert(e);
+    }
+}
+BENCHMARK_REGISTER_F(UniqueFixture, Vector_Remove_Dup)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({128}) // 1KB
+    ->Args({10*128}) // 10KB
+    ->Args({100*128}) // 100KB
+    ->Args({128*1024}) // 1MB
+    ->Args({10*128*1024}); // 10MB
+    //->Args({100*128*1024}) // 100MB
+    //->Args({128*1024*1024}); // 1GB
+
+// using a unique list
+BENCHMARK_DEFINE_F(UniqueFixture, List_Remove_Dup)(benchmark::State& state) {
+    Container<std::size_t, std::list, Unique> c;
+    while (state.KeepRunning()) {
+        for (size_t e : data)
+            c.insert(e);
+    }
+}
+BENCHMARK_REGISTER_F(UniqueFixture, List_Remove_Dup)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({128}) // 1KB
+    ->Args({10*128}) // 10KB
+    ->Args({100*128}) // 100KB
+    ->Args({128*1024}) // 1MB
+    ->Args({10*128*1024}); // 10MB
+    //->Args({100*128*1024}) // 100MB
+    //->Args({128*1024*1024}); // 1GB
+
+BENCHMARK_MAIN();
