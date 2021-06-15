c169e6ab726ba4dfb6b11e4f693d7f5ddc29b1ca
Xueying Qin <qinxy1995@gmail.com>
2021-06-15 15:35:21 +0100

    add output file

--- cpp_containers/run.sh
+++ cpp_containers/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-./build/benchmark_o2
+mkdir -p results
 
-./build/benchmark_o3
+./build/benchmark_o2 --benchmark_out=./results/benchmark_o2.json --benchmark_out_format=json
+
+./build/benchmark_o3 --benchmark_out=./results/benchmark_o3.json --benchmark_out_format=json
