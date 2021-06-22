433b8e35159f2c34d406baff758bf9ab637b8331
Xueying Qin <qinxy1995@gmail.com>
2021-06-22 15:20:15 +0100

    Add random pair generator

--- cpp_containers/rng.hpp
+++ cpp_containers/rng.hpp
@@ -18,3 +18,33 @@ void Generate(Container<T, C, Ps...>& c, std::size_t size) {
     for (std::size_t i=0; i < size; i++)
         c.insert((std::size_t)generator());
 }
+
+std::pair<std::size_t, std::string> generate_pair(std::size_t len, std::size_t seed) {
+    static constexpr auto chars =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::minstd_rand generator;
+    generator.seed(seed);
+    auto dist = std::uniform_int_distribution{{}, std::strlen(chars) - 1};
+    auto result = std::string(len, '\0');
+    std::generate_n(begin(result), len, [&]() { return chars[dist(generator)]; });
+    return std::pair<std::size_t, std::string>((std::size_t)generator(), result);
+}
+
+std::vector<std::pair<std::size_t, std::string>> generate_pairs(std::size_t len, std::size_t size) {
+    std::vector<std::pair<std::size_t, std::string>> v;
+    static constexpr auto chars =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::minstd_rand generator;
+    generator.seed(size);
+    for (std::size_t i=0; i < size; i++) {
+        auto dist = std::uniform_int_distribution{{}, std::strlen(chars) - 1};
+        auto result = std::string(len, '\0');
+        std::generate_n(begin(result), len, [&]() { return chars[dist(generator)]; });
+        v.push_back(std::pair<std::size_t, std::string>((std::size_t)generator(), result));
+    }
+    return v;
+}
