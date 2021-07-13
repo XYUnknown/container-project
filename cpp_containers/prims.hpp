7f213431c2491de5d094ed173c1d1ea507fa2128
Xueying Qin <qinxy1995@gmail.com>
2021-07-13 01:19:28 +0100

    Update prim's

--- cpp_containers/prims.hpp
+++ cpp_containers/prims.hpp
@@ -2,7 +2,16 @@
 #include <vector>
 #include <string>
 #include <list>
+ 
+#include <limits> // for numeric_limits
+
 #include <queue>
+#include <utility> // for pair
+#include <fstream> // read graph data
+#include <algorithm>
+#include <iterator>
+
+#include "containers.hpp"
 
 typedef int vertex_t;
 typedef double weight_t;
@@ -19,40 +28,175 @@ struct neighbor {
 typedef std::vector<std::vector<neighbor>> adjacency_list_t;
 typedef std::pair<weight_t, vertex_t> weight_vertex_pair_t;
 
-template<class C>
-void prims(const adjacency_list_t &adj_list, 
-           std::vector<bool> &visited, 
-           std::vector<vertex_t> &connection, 
-           std::vector<weight_t> &value) {
-    C que;
-    que.push(std::make_pair(0, 0));  //push the weight required to insert the source node =0 and the node itself(i.e 1)
-    value[0]=0;                 //minimum weight for source is 0  
-    while (!que.empty()) {      
-        vertex_t node = que.top().second;  //get the node
-        visited[node] = true;         //as it is visited now change its value to true
-        que.pop();                    
-        for (auto neighbor : adj_list[node]) {   //we check for all its neighbors
-            weight_t weight = neighbor.weight;        //get their weight
-            vertex_t vertex = neighbor.target;         //get their index
-
-            if (!visited[vertex] && value[vertex] > weight) {   //if the node is not visited and if its weight along this edge is less than the 
-                value[vertex] = weight;                         //previous edge associated with it, then only we consider it
-                connection[vertex] = node;
-                que.push(std::make_pair(value[vertex], vertex));     //we update the values and then push it in the queue to examine its neighbors
-            }
-        }
-    }
-}
-
-void print_graph(std::vector<vertex_t> &connection) {
-    for (int i = 1; i < 6; ++i)
-        printf("%d - %d\n", connection[i], i);  //print the connections
-}
-
 void makegraph(int m, int n, weight_t wght, adjacency_list_t &adj_list) {
 
     /* This function adds the edges and nodes to
       the graph in the form of an adjacency list */
     adj_list[m].push_back(neighbor(n, wght));     //make a pair of the node and its weight
     adj_list[n].push_back(neighbor(m, wght));     //we need to add it both ways i.e if a connects b, then b also connects a
+}
+
+adjacency_list_t constructGraph(std::string filename) {
+    // Open the file:
+    std::ifstream fin(filename);
+
+    // Declare variables:
+    int M, N, L;
+
+    // Ignore headers and comments:
+    while (fin.peek() == '%') fin.ignore(2048, '\n');
+
+    // Read defining parameters:
+    fin >> M >> N >> L;
+
+    adjacency_list_t adjacency_list(M);
+
+    // Read the data
+    for (int l = 0; l < L; l++)
+    {
+        int m, n;
+        double data;
+        fin >> m >> n >> data;
+        makegraph(m-1, n-1, data, adjacency_list);
+    }
+
+    fin.close();
+    return adjacency_list;
+}
+
+std::pair<std::size_t, std::size_t> graphInfo(adjacency_list_t l) {
+    auto numV = l.size();
+    auto numE = 0;
+    for (std::vector<neighbor> n : l) {
+        numE += n.size();
+    }
+    return std::pair(numV, numE);
+}
+
+void printAdjList(adjacency_list_t l) {
+    auto info = graphInfo(l);
+    std::cout << "Number of vertices: " << info.first << " Number of edges: " << info.second << std::endl;
+    for (auto i=0; i < l.size(); i++) {
+        for (neighbor n : l[i]) {
+            std::cout << "edge: {" << i << ", " << n.target << "} weight: " << n.weight << std::endl;
+        }
+    }
+}
+
+template<class C>
+void prims(const adjacency_list_t &adj_list, 
+           std::vector<bool> &visited, 
+           std::vector<vertex_t> &connection, 
+           std::vector<weight_t> &value) {
+    int n = adj_list.size();
+    visited.clear();
+    visited.resize(n, false);
+    connection.clear();
+    connection.resize(n, -1);
+    value.clear();
+    value.resize(n, max_weight);
+    C que;
+    if constexpr (std::is_same<C, std::priority_queue<weight_vertex_pair_t, std::vector<weight_vertex_pair_t>, std::greater<weight_vertex_pair_t>>>::value) {
+        que.push(std::make_pair(0, 0));  //push the weight required to insert the source node =0 and the node itself(i.e 1)
+        value[0] = 0;                 //minimum weight for source is 0  
+        while (!que.empty()) {      
+            vertex_t node = que.top().second;  //get the node
+            que.pop();
+            if(visited[node] == true){
+                continue;
+            }
+            visited[node] = true;
+            const std::vector<neighbor> &neighbors = adj_list[node];
+            for (std::vector<neighbor>::const_iterator neighbor_iter = neighbors.begin();
+                neighbor_iter != neighbors.end();
+                neighbor_iter++) {   //we check for all its neighbors
+                weight_t weight = neighbor_iter->weight;        //get their weight
+                vertex_t vertex = neighbor_iter->target;         //get their index
+
+                if (!visited[vertex] && value[vertex] > weight) {   //if the node is not visited and if its weight along this edge is less than the 
+                    value[vertex] = weight;                         //previous edge associated with it, then only we consider it
+                    connection[vertex] = node;
+                    que.push(std::make_pair(value[vertex], vertex));     //we update the values and then push it in the queue to examine its neighbors
+                }
+            }
+        }
+    } else if constexpr (std::is_same<C, Container<weight_vertex_pair_t, TreeWrapper>>::value) { // balanced binary search tree
+        que.insert(std::make_pair(0.0, 0));  //push the weight required to insert the source node =0 and the node itself(i.e 1)
+        value[0] = 0.0;                 //minimum weight for source is 0  
+        while (!que.empty()) {      
+            vertex_t node = que.begin()->second;  //get the node
+            que.erase(que.begin());
+            if(visited[node] == true){
+                continue;
+            }
+            visited[node] = true;           
+            const std::vector<neighbor> &neighbors = adj_list[node];
+            for (std::vector<neighbor>::const_iterator neighbor_iter = neighbors.begin();
+                neighbor_iter != neighbors.end();
+                neighbor_iter++) {   //we check for all its neighbors
+                weight_t weight = neighbor_iter->weight;        //get their weight
+                vertex_t vertex = neighbor_iter->target;        //get their index
+
+                if (!visited[vertex] && value[vertex] > weight) {   //if the node is not visited and if its weight along this edge is less than the 
+                    value[vertex] = weight;                         //previous edge associated with it, then only we consider it
+                    connection[vertex] = node;
+                    que.insert(std::make_pair(value[vertex], vertex));     //we update the values and then push it in the queue to examine its neighbors
+                }
+            }
+        }
+    } else if constexpr (std::is_same<C, Container<weight_vertex_pair_t, std::vector>>::value) {
+        que.push_back(std::make_pair(0.0, 0));  //push the weight required to insert the source node =0 and the node itself(i.e 1)
+        value[0] = 0.0;                 //minimum weight for source is 0  
+        while (!que.empty()) {   
+            auto min = std::min_element(que.begin(), que.end());   
+            vertex_t node = min->second;  //get the node
+            que.erase(min);
+            if(visited[node] == true){
+                continue;
+            }
+            visited[node] = true;           
+            const std::vector<neighbor> &neighbors = adj_list[node];
+            for (std::vector<neighbor>::const_iterator neighbor_iter = neighbors.begin();
+                neighbor_iter != neighbors.end();
+                neighbor_iter++) {   //we check for all its neighbors
+                weight_t weight = neighbor_iter->weight;        //get their weight
+                vertex_t vertex = neighbor_iter->target;        //get their index
+
+                if (!visited[vertex] && value[vertex] > weight) {   //if the node is not visited and if its weight along this edge is less than the 
+                    value[vertex] = weight;                         //previous edge associated with it, then only we consider it
+                    connection[vertex] = node;
+                    que.push_back(std::make_pair(value[vertex], vertex));     //we update the values and then push it in the queue to examine its neighbors
+                }
+            }
+        }
+    } else {
+        que.insert(std::make_pair(0.0, 0));  //push the weight required to insert the source node =0 and the node itself(i.e 1)
+        value[0] = 0.0;                 //minimum weight for source is 0  
+        while (!que.empty()) {      
+            vertex_t node = que.back().second;  //get the node
+            que.pop_back();
+            if(visited[node] == true){
+                continue;
+            }
+            visited[node] = true;                    
+            const std::vector<neighbor> &neighbors = adj_list[node];
+            for (std::vector<neighbor>::const_iterator neighbor_iter = neighbors.begin();
+                neighbor_iter != neighbors.end();
+                neighbor_iter++) {   //we check for all its neighbors
+                weight_t weight = neighbor_iter->weight;        //get their weight
+                vertex_t vertex = neighbor_iter->target;        //get their index
+
+                if (!visited[vertex] && value[vertex] > weight) {   //if the node is not visited and if its weight along this edge is less than the 
+                    value[vertex] = weight;                         //previous edge associated with it, then only we consider it
+                    connection[vertex] = node;
+                    que.insert(std::make_pair(value[vertex], vertex));     //we update the values and then push it in the queue to examine its neighbors
+                }
+            }
+        }
+    }
+}
+
+void print_graph(std::vector<vertex_t> &connection) {
+    for (int i = 1; i < connection.size(); ++i)
+        printf("%d - %d\n", connection[i], i);  //print the connections
 }
--- cpp_containers/prims_examples.cpp
+++ cpp_containers/prims_examples.cpp
@@ -3,21 +3,27 @@
 int main() {
     int V = 6;
     adjacency_list_t adj_list(V); //to make the graph
-    std::vector<bool> visited(V,false);  // to keep a track if the node has been already visited or not. Initially all are false as no node is visited
-    std::vector<vertex_t> connection(V, -1);  // to track the final connections that the MST has
-    std::vector<weight_t> value (V, max_weight); // to store the minimum weight value possible for a node
+    std::vector<bool> visited; // to keep a track if the node has been already visited or not. Initially all are false as no node is visited
+    std::vector<vertex_t> connection; // to track the final connections that the MST has
+    std::vector<weight_t> value; // to store the minimum weight value possible for a node
 
-    makegraph(4, 3, 5, adj_list);   //insert the node and edge
-    makegraph(4, 0, 3, adj_list);   //insert the node and edge
-    makegraph(0, 1, 3, adj_list);   //insert the node and edge
-    makegraph(0, 3, 7, adj_list);   //insert the node and edge
-    makegraph(1, 4, 11, adj_list);  //insert the node and edge
-    makegraph(5, 3, 1, adj_list);   //insert the node and edge
-    makegraph(4, 5, 7, adj_list);   //insert the node and edge
-    makegraph(2, 0, 3, adj_list);   //insert the node and edge
-    makegraph(2, 1, 7, adj_list);   //insert the node and edge
+    //insert the node and edge
+    makegraph(4, 3, 5, adj_list);   
+    makegraph(4, 0, 3, adj_list); 
+    makegraph(0, 1, 3, adj_list);
+    makegraph(0, 3, 7, adj_list);
+    makegraph(1, 4, 11, adj_list);
+    makegraph(5, 3, 1, adj_list);
+    makegraph(4, 5, 7, adj_list);
+    makegraph(2, 0, 3, adj_list);
+    makegraph(2, 1, 7, adj_list);
 
-    prims<std::priority_queue<weight_vertex_pair_t, std::vector<weight_vertex_pair_t>, std::greater<weight_vertex_pair_t>>>(adj_list, visited, connection, value);  //call the function the perform the minimum spanning tree algorithm
+    adjacency_list_t adjList = constructGraph("./matrix/uw800s.mtx");
+    //printAdjList(adjList);
+    //prims<std::priority_queue<weight_vertex_pair_t, std::vector<weight_vertex_pair_t>, std::greater<weight_vertex_pair_t>>>(adjList, visited, connection, value);  //call the function the perform the minimum spanning tree algorithm
+    //prims<Container<weight_vertex_pair_t, TreeWrapper>>(adj_list, visited, connection, value); 
+    //prims<Container<weight_vertex_pair_t, std::vector, SortedOnAccess<weight_vertex_pair_t, std::greater<weight_vertex_pair_t>>>>(adjList, visited, connection, value); 
+    prims<Container<weight_vertex_pair_t, std::vector>>(adj_list, visited, connection, value); 
     print_graph(connection);        //print the final minimum spanning tree
 
     return 0;
