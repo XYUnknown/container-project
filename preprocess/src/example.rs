d9a1f778a10ec003d2af7208eadc8fac05be6b66
Xueying Qin <qinxy1995@gmail.com>
2021-11-09 14:41:39 +0000

    Add expected input and output

---
+++ preprocess/src/example.rs
@@ -0,0 +1,20 @@
+
+
+/*SPEC*
+property unique {
+    \c -> for_all_unique_pairs c (\a -> \b -> neq a b)
+}
+type UniqueCon<T> = {c : Con<T> | unique c}
+*ENDSPEC*/
+
+
+
+fn main () {
+    let mut c = UniqueCon::<u32>::new();
+    for x in 0..10 {
+        c.insert(x);
+        c.insert(x);
+    }
+    assert_eq!(c.len(), 10);
+}
+
---
+++ preprocess/src/example_lib_spec.rs
@@ -0,0 +1,15 @@
+/*LIBRARY_SPEC*
+std::collections::HashSet<T> {
+    isUnique = true
+}
+
+
+std::collections::BTreeSet<T> {
+    isUnique = true
+}
+
+
+std::collections::Vec<T> {
+    isUnique = false
+}
+*END_LIBRARY_SPEC*/
