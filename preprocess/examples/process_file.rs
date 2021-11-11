64bda857301881c956cf3a1d2bcc25dbd4134af8
Xueying Qin <qinxy1995@gmail.com>
2021-11-11 16:38:36 +0000

    Add a naive specification processing with code generation

--- preprocess/examples/process_file.rs
+++ preprocess/examples/process_file.rs
@@ -1,7 +1,17 @@
-use preprocess::parser::{spec};
-use preprocess::generator::{readfile};
+use preprocess::parser::{Decl, spec};
+use preprocess::generator::{readfile, writefile, process_src};
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
 
 fn main() {
-    let f = readfile("./spec_code/example.rs".to_string());
-    println!("{:?}", spec::prog(&f));
+    //let f = readfile("./spec_code/example.rs".to_string());
+    //println!("{:?}", spec::prog(&f));
+    //println!("{}", type_of(spec::prog(&f)));
+    //println!("{}", process_spec(Vec::<Decl>::new()));
+    println!("{}", process_src("./spec_code/example.rs".to_string()));
+    writefile("example_output_hashset.rs".to_string(), process_src("./spec_code/example.rs".to_string()));
+
 }
--- preprocess/src/generator.rs
+++ preprocess/src/generator.rs
@@ -1,7 +1,11 @@
 use std::env;
 use std::fs;
+use std::io::{Write, BufReader, BufRead, Error};
 
-use crate::parser::{Id, Type, spec};
+use crate::parser::{Block, Spec, spec};
+
+const CODEGEN: &str = "/*CODEGEN*/\n";
+const CODEGENEND: &str = "\n/*CODEGENEND*/\n";
 
 pub fn readfile(filename : String) -> String {
     let contents = fs::read_to_string(filename)
@@ -9,10 +13,51 @@ pub fn readfile(filename : String) -> String {
     contents
 }
 
+pub fn writefile(filename : String, contents: String) -> Result<(), Error> {
+    let path = "./gen_code/";
+
+    let mut output = fs::File::create(path.to_owned() + &filename)?;
+    write!(output, "{}", contents)?;
+
+    Ok(())
+}
+
+pub fn extract_code(block: Block) -> String {
+    match block {
+        Block::SpecBlock(spec, n) => {
+            process_spec(spec.to_vec())
+        },
+        Block::CodeBlock(code, n) => {
+            code.to_string()
+        }
+    }
+}
+
+pub fn process_spec(s: Spec) -> String {
+    // TODO : actually process the spec
+    let code: &str = "type UniqueCon<T> = std::collections::HashSet<T>;";
+    let result = CODEGEN.to_owned() + code + CODEGENEND;
+    result
+}
+
+pub fn process_src(filename : String) -> String {
+    let f = readfile("./spec_code/example.rs".to_string());
+    match spec::prog(&f) {
+        Ok(blocks) => {
+            let mut result = String::new();
+            for block in blocks.iter() {
+                result = result + &extract_code(block.to_owned());
+            }
+            result
+        },
+        _ => "Error, invalid source code.".to_string()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::generator::{readfile};
-    use crate::parser::{spec};
+    use crate::generator::{readfile, process_spec};
+    use crate::parser::{Decl, spec};
 
     #[test]
     fn test_read_file() {
@@ -25,5 +70,10 @@ mod tests {
     fn test_parse_file() {
         let f = readfile("./spec_code/example.rs".to_string());
         assert!(spec::prog(&f).is_ok())
+    }
+
+    #[test]
+    fn test_process_spec() {
+        assert!(process_spec(Vec::<Decl>::new()).len() != 0);
     }
 }
