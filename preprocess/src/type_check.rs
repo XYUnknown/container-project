1bb051e3c1f275da6bdf97d1447d6a6fd0cc30ba
Xueying Qin <qinxy1995@gmail.com>
2021-11-23 14:04:52 +0000

    Update type checker

--- preprocess/src/type_check.rs
+++ preprocess/src/type_check.rs
@@ -1,28 +1,41 @@
-use crate::parser::{Prog, Block, Spec, Decl, Type, spec};
-use crate::symbol_table::{SymbolTable};
+use crate::parser::{Prog, Block, Spec, Decl, Type, Term, spec};
+use crate::ctx::{Ctx};
 use crate::generator::{readfile};
 
 type TypeError = String;
 type TypeCheckResult<T> = Result<T, TypeError>;
 
 pub struct TypeChecker {
-    table : SymbolTable,
+    global_ctx : Ctx,
 }
 
 impl TypeChecker {
     pub fn new() -> TypeChecker {
         TypeChecker {
-            table: SymbolTable::new(),
+            global_ctx: Ctx::new(),
         }
     }
 
+    fn predefined(&mut self) {
+        // put for_all_unique_pair into context
+        let args0 = vec![Type::Ty(Box::new("T".to_string())), Type::Ty(Box::new("T".to_string()))];
+        let binary_fn = Type::Fun(Box::new(args0), Box::new(Type::Bool()));
+        let args1 = vec![Type::Ty(Box::new("Con<T>".to_string())), binary_fn];
+        self.global_ctx.put("for_all_unique_pair".to_string(), Type::Fun(Box::new(args1), Box::new(Type::Bool())));
+
+        // put neq into context
+        let args2 = vec![Type::Ty(Box::new("T".to_string())), Type::Ty(Box::new("T".to_string()))];
+        let neq_fn = Type::Fun(Box::new(args2), Box::new(Type::Bool()));
+        self.global_ctx.put("neq".to_string(), neq_fn);
+    }
+
     pub fn check_prog(&mut self, prog: Prog) -> Result<(), TypeError> {
-        // TODO: enter predefined functions into the symbol table
         let specs: Vec<Spec> = 
             prog.iter()
             .filter(| block | block.is_spec_block())
             .map(| block | block.extract_spec())
             .collect();
+        self.predefined();
         self.check_specs(specs)
     }
 
@@ -55,16 +68,28 @@ impl TypeChecker {
 
     pub fn check_prop_decl(&mut self, decl: &Decl) -> Result<(), TypeError> {
         // No duplication
-        // check well formedness
-        // type checking
         match decl {
             Decl::PropertyDecl(id, term) => {
                 // Duplicate property decl checking
-                match self.table.get_id(id.to_string()) {
+                match self.global_ctx.get_id(id.to_string()) {
                     Some(_) => Err("Duplicate property declaration".to_string()),
                     None => {
-                        self.table.put(id.to_string(), Type::Bool()); // Should be: put(id, check_term(term))
-                        Ok(())
+                        // check well formedness
+                        // it should have type Con<T> -> Bool
+                        // otherwise error
+                        let mut ctx = Ctx::new();
+                        match self.check_term(&mut ctx, term) {
+                            Ok(t) => {
+                                let desired_prop_type = Type::Fun(Box::new(vec![Type::Ty(Box::new("Con<T>".to_string()))]), Box::new(Type::Bool()));
+                                if t == &desired_prop_type {
+                                    self.global_ctx.put(id.to_string(), desired_prop_type);
+                                    Ok(())
+                                } else {
+                                    Err("Not a valid property type".to_string())
+                                }
+                            },
+                            Err(e) => Err(e)
+                        }
                     },
                 }
             },
@@ -87,7 +112,33 @@ impl TypeChecker {
         Ok(())
     }
 
-    pub fn check_term() {
+    pub fn check_term<'a>(&mut self, ctx: &'a mut Ctx, term: &Term) -> Result<&'a Type, TypeError> {
+        // TODO: Add proper property type checking later
+        let placeholder_type = Type::Fun(Box::new(vec![Type::Ty(Box::new("Con<T>".to_string()))]), Box::new(Type::Bool()));
+        ctx.put("placeholder_type".to_string(), placeholder_type);
+        match term {
+            Term::VarTerm(id) => {
+                // Type checking placeholder
+                match ctx.get_id("placeholder_type".to_string()) {
+                    Some(t) => Ok(t),
+                    None => Err("Undefined variable".to_string())
+                }
+            },
+            Term::LambdaTerm(id, term) => {
+                // Type checking placeholder
+                match ctx.get_id("placeholder_type".to_string()) {
+                    Some(t) => Ok(t),
+                    None => Err("Undefined variable".to_string())
+                }
+            },
+            Term::AppTerm(term1, term2) => {
+                // Type checking placeholder
+                match ctx.get_id("placeholder_type".to_string()) {
+                    Some(t) => Ok(t),
+                    None => Err("Undefined variable".to_string())
+                }
+            }
+        }
     }
 }
 
