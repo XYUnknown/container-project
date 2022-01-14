82f1ace8847a62ebcea5020e386f95a38bcb5620
Xueying Qin <qinxy1995@gmail.com>
2022-01-14 16:56:52 +0000

    Modify property-spec match checking

--- match-specs/sat-check.rkt
+++ match-specs/sat-check.rkt
@@ -3,26 +3,31 @@
 (require "list.rkt")
 (require "unique-list.rkt")
 (require "ascending-list.rkt")
+(require "strict-ascending-list.rkt")
 (require "utils.rkt")
 
-(define (check-spec-length prop spec xs)
-  (prop (cdr (spec xs))))
+(define (check-spec-length prop pre spec xs)
+  (assume (and (prop xs) (pre xs)))
+  (assert (prop (cdr (spec xs)))))
 
-(define (check-spec-contains prop spec x xs)
-  (prop (cdr (spec x xs))))
+(define (check-spec-contains prop pre spec x xs)
+  (assume (and (prop xs) (pre xs)))
+  (assert (prop (cdr (spec x xs)))))
 
-(define (check-spec-insert prop spec x xs)
-  (prop (spec x xs)))
+(define (check-spec-insert prop pre spec x xs)
+  (assume (and (prop xs) (pre xs)))
+  (assert (prop (spec x xs))))
 
-(define (check-spec-remove prop spec x xs)
-  (prop (spec x xs)))
+(define (check-spec-remove prop pre spec x xs)
+  (assume (and (prop xs) (pre xs)))
+  (assert (prop (spec x xs))))
 
-(define (check prop specs x xs)
-  (assume (prop xs))
-  (assert (and (check-spec-length prop (first specs) xs)
-               (check-spec-contains prop (second specs) x xs)
-               (check-spec-insert prop (third specs) x xs)
-               (check-spec-remove prop (fourth specs) x xs))))
+(define (check prop pres specs x xs)
+  (and (unsat? (verify (check-spec-length prop (first pres) (first specs) xs)))
+       (unsat? (verify (check-spec-contains prop (second pres) (second specs) x xs)))
+       (unsat? (verify (check-spec-insert prop (third pres) (third specs) x xs)))
+       (unsat? (verify (check-spec-remove prop (fourth pres) (fourth specs) x xs)))))
+
 
 ; The list
 (define-symbolic elem integer?)
@@ -30,25 +35,35 @@
 (define ls (take-bv (generate-list 10) len))
 
 ; Uncomment to run
-; (verify (check unique (list spec-length spec-contains spec-insert spec-remove) elem ls))
+; (check unique (list pre-length pre-contains pre-insert pre-remove) (list spec-length spec-contains spec-insert spec-remove) elem ls)
 ; model found
 
-; (verify (check ascending (list spec-length spec-contains spec-insert spec-remove) elem ls))
+; (check ascending (list pre-length pre-contains pre-insert pre-remove) (list spec-length spec-contains spec-insert spec-remove) elem ls)
 ; model found
 
-; (verify (check unique (list spec-length-unique spec-contains-unique spec-insert-unique spec-remove-unique) elem ls))
+; (check unique (list pre-length-unique pre-contains-unique pre-insert-unique pre-remove-unique) (list spec-length-unique spec-contains-unique spec-insert-unique spec-remove-unique) elem ls)
 ; (unsat)
 
-; (verify (check ascending (list spec-length-unique spec-contains-unique spec-insert-unique spec-remove-unique) elem ls))
+; (check ascending (list pre-length-unique pre-contains-unique pre-insert-unique pre-remove-unique) (list spec-length-unique spec-contains-unique spec-insert-unique spec-remove-unique) elem ls)
 ; model found
 
-; (verify (check (lambda (x) (and (unique x) (ascending x))) (list spec-length-unique spec-contains-unique spec-insert-unique spec-remove-unique) elem ls))
+(check (lambda (x) (and (unique x) (ascending x))) (list pre-length-unique pre-contains-unique pre-insert-unique pre-remove-unique) (list spec-length-unique spec-contains-unique spec-insert-unique spec-remove-unique) elem ls)
 ; model found
 
-; (verify (check ascending (list spec-length-ascending spec-contains-ascending spec-insert-ascending spec-remove-ascending) elem ls))
+; (check ascending (list pre-length-ascending pre-contains-ascending pre-insert-ascending pre-remove-ascending) (list spec-length-ascending spec-contains-ascending spec-insert-ascending spec-remove-ascending) elem ls)
 ; (unsat)
 
-; (verify (check unique (list spec-length-ascending spec-contains-ascending spec-insert-ascending spec-remove-ascending) elem ls))
-; (model found)
+; (check unique (list pre-length-ascending pre-contains-ascending pre-insert-ascending pre-remove-ascending) (list spec-length-ascending spec-contains-ascending spec-insert-ascending spec-remove-ascending) elem ls)
+; model found
 
-(verify (check (lambda (x) (and (unique x) (ascending x))) (list spec-length-ascending spec-contains-ascending spec-insert-ascending spec-remove-ascending) elem ls))
+; (check (lambda (x) (and (unique x) (ascending x))) (list pre-length-ascending pre-contains-ascending pre-insert-ascending pre-remove-ascending) (list spec-length-ascending spec-contains-ascending spec-insert-ascending spec-remove-ascending) elem ls)
+; model found
+
+; (check (lambda (x) (and (unique x) (ascending x))) (list pre-length-strict-ascending pre-contains-strict-ascending pre-insert-strict-ascending pre-remove-strict-ascending) (list spec-length-strict-ascending spec-contains-strict-ascending spec-insert-strict-ascending spec-remove-strict-ascending) elem ls)
+; (unsat)
+
+; (check unique (list pre-length-strict-ascending pre-contains-strict-ascending pre-insert-strict-ascending pre-remove-strict-ascending) (list spec-length-strict-ascending spec-contains-strict-ascending spec-insert-strict-ascending spec-remove-strict-ascending) elem ls)
+; (unsat)
+
+; (check ascending (list pre-length-strict-ascending pre-contains-strict-ascending pre-insert-strict-ascending pre-remove-strict-ascending) (list spec-length-strict-ascending spec-contains-strict-ascending spec-insert-strict-ascending spec-remove-strict-ascending) elem ls)
+; (unsat)
