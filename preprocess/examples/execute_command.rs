3ebce60412af3ec46f84ace2dffb5adcf9d6a816
Xueying Qin <qinxy1995@gmail.com>
2022-02-05 19:40:50 +0000

    Update example

--- preprocess/examples/execute_command.rs
+++ preprocess/examples/execute_command.rs
@@ -1,7 +1,9 @@
-use preprocess::run_matching::{run_matching, gen_match_script};
+use preprocess::run_matching::{run_matching, gen_match_script, cleanup_script, setup_dirs};
 fn main() {
+    //setup_dirs();
     //let result = run_matching("./racket_specs/gen_match/example-match.rkt".to_string());
     //println!("{:?}", result);
     let match_file = gen_match_script("unique".to_string(), "unique.rkt".to_string(), "rust-list-spec.rkt".to_string());
     println!("{:?}", match_file);
+    //cleanup_script();
 }
