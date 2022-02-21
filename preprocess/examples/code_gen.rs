367c17b04ca378761ee022fff08cf701877e61f1
Xueying Qin <qinxy1995@gmail.com>
2022-02-21 20:29:39 +0000

    clean up example

--- preprocess/examples/code_gen.rs
+++ preprocess/examples/code_gen.rs
@@ -1,4 +1,4 @@
 use preprocess::generator::{gen_output_code};
 fn main() {
-    println!("{}", gen_output_code("UniqueCon<T>", "T", "std::collections::BTreeSet::<T>", "trait UniqueConTrait<T> : preprocess::traits::container::Container<T>", "UniqueConTrait<T>", "none"));
+    println!("{}", gen_output_code("UniqueCon", "T", "std::collections::BTreeSet", "UniqueConTrait", "none"));
 }
