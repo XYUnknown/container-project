8249f6d4f17463a9c2146eba8d8cd0b7f64c4e07
Xueying Qin <qinxy1995@gmail.com>
2022-02-23 16:20:25 +0000

    Update example

--- preprocess/spec_code/example.rs
+++ preprocess/spec_code/example.rs
@@ -10,7 +10,7 @@ type UniqueCon<T> = {c impl (Container) | (unique c) }
 property ascending {
     \c -> ((for-all-consecutive-pairs c) leq?)
 }
-type AscendingCon<T> = {c impl (Container) | (ascending c)}
+type AscendingConWithPos<T> = {c impl (Container, WithPosition) | (ascending c)}
 
 type UniqueAscendingCon<T> = {c impl (Container) | ((unique c) and (ascending c)) }
 *ENDSPEC*/
