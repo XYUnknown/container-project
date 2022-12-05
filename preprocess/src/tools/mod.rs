745ec1ee6b12029a20594034e0c436a60809319f
Xueying Qin <qinxy1995@gmail.com>
2022-12-05 18:45:53 +0000

    Add code for generating data set for memory profiling

--- preprocess/src/tools/mod.rs
+++ preprocess/src/tools/mod.rs
@@ -6,38 +6,37 @@ use rand::rngs::StdRng;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-// one search at the 
-pub fn gen_dataset_1() -> Vec<u32> { // avoid duplication
+pub fn gen_dataset_1() -> Vec<u32> {
     let size = 1024 * 1024; // 1 MB
     let amount = size / size_of::<u32>();
-    let mut data: Vec<u32> = (1..amount as u32).collect(); //ensure no duplication
+    let mut data: Vec<u32> = (1..amount as u32).collect();
     let mut rng = StdRng::seed_from_u64(222);
     data.shuffle(&mut rng);
     data
 }
 
-pub fn gen_dataset_128() -> Vec<u32> { // avoid duplication
+pub fn gen_dataset_128() -> Vec<u32> {
     let size = 128 * 1024 * 1024; // 128 MB
     let amount = size / size_of::<u32>();
-    let mut data: Vec<u32> = (1..amount as u32).collect(); //ensure no duplication
+    let mut data: Vec<u32> = (1..amount as u32).collect();
     let mut rng = StdRng::seed_from_u64(222);
     data.shuffle(&mut rng);
     data
 }
 
-pub fn gen_dataset_256() -> Vec<u32> { // avoid duplication
+pub fn gen_dataset_256() -> Vec<u32> {
     let size = 256 * 1024 * 1024; // 256 MB
     let amount = size / size_of::<u32>();
-    let mut data: Vec<u32> = (1..amount as u32).collect(); //ensure no duplication
+    let mut data: Vec<u32> = (1..amount as u32).collect();
     let mut rng = StdRng::seed_from_u64(222);
     data.shuffle(&mut rng);
     data
 }
 
-pub fn gen_dataset_512() -> Vec<u32> { // avoid duplication
+pub fn gen_dataset_512() -> Vec<u32> {
     let size = 512 * 1024 * 1024; // 512 MB
     let amount = size / size_of::<u32>();
-    let mut data: Vec<u32> = (1..amount as u32).collect(); //ensure no duplication
+    let mut data: Vec<u32> = (1..amount as u32).collect();
     let mut rng = StdRng::seed_from_u64(222);
     data.shuffle(&mut rng);
     data
